5b1784be30ba53f0f36a43e74e142f2469f924fe
Dmitry <kropotovd@gmail.com>
2017-12-03 01:16:45 +0300

    I have just created new repository and now I am committing src files

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        animal cat = new animal();
+        cat.x=1;
+        cat.y=2;
+        System.out.println("Hello, world. "+cat.x+"+"+cat.y+"="+cat.sum());
+    }
+}
